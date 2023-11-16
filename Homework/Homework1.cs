306e2332f543256366d34b7f9836e1f4987962ce
kachiZ <lihourouk@gmail.com>
2023-11-16 20:05:45 +0700

    Add project files.

---
+++ Homework/Homework1.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework
+{
+    internal class Homework1
+    {
+        private int num1, num2;
+
+        public int sum(int lh, int rh)
+        {
+            return (lh + rh);
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter 2 Integer to Sum:");
+                    Console.Write("Enter Integer 1: ");
+                    num1 = int.Parse(Console.ReadLine());
+                    Console.Write("Enter Integer 2: ");
+                    num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("The sum is: {0}", sum(num1, num2));
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Input. Only Integer is accepted");
+                }
+            }
+        }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Homework
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to C# Programming");
+            Homework1 homework1 = new Homework1();
+            homework1.run();
+        }
+    }
+}
